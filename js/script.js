964cfa4729b418823e24d77c191930281bfbfe01
r4sk01 <limbus727@gmail.com>
2022-06-27 22:02:41 -0600

    Now user can switch between 4 tabs. Fade animation added for switching tabs.

---
+++ js/script.js
@@ -0,0 +1,39 @@
+window.addEventListener("DOMContentLoaded", () => {
+  console.log("DOM Content Loaded");
+
+  const tabs = document.querySelectorAll(".tabheader__item"),
+    tabsContent = document.querySelectorAll(".tabcontent"),
+    tabsParent = document.querySelector(".tabheader__items");
+
+  function hideTabContent() {
+    tabsContent.forEach((item) => {
+      item.classList.remove("show", "fade");
+      item.classList.add("hide");
+    });
+
+    tabs.forEach((item) => {
+      item.classList.remove("tabheader__item_active");
+    });
+  }
+
+  function showTabContent(i = 0) {
+    tabsContent[i].classList.remove("hide");
+    tabsContent[i].classList.add("show", "fade");
+    tabs[i].classList.add("tabheader__item_active");
+  }
+
+  tabsParent.addEventListener("click", (event) => {
+    if (event.target && event.target.matches("div.tabheader__item")) {
+      tabs.forEach((item, i) => {
+        if (event.target == item) {
+          hideTabContent();
+          showTabContent(i);
+        }
+      });
+    }
+  });
+
+  // initial init
+  hideTabContent();
+  showTabContent();
+});
