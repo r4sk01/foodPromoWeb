93365ebc5f56d6e3d05315ad2f40c862ee23c51e
r4sk01 <limbus727@gmail.com>
2022-06-30 22:54:13 -0600

    Added Modal Window for event when user click on 'contact us' button.

--- js/script.js
+++ js/script.js
@@ -104,4 +104,48 @@ window.addEventListener("DOMContentLoaded", () => {
   }
 
   setClock(".timer", deadline);
+
+  // Open and Close Promotion Modal Window
+
+  const modalOpenBtns = document.querySelectorAll("[data-modal]"),
+    modal = document.querySelector(".modal"),
+    modalCloseBtn = document.querySelector("[data-close]");
+
+  function openModal() {
+    modal.classList.add("show");
+    modal.classList.remove("hide");
+    //modal.classList.toggle("show");
+    // do not allow scrool when modal is up
+    document.body.style.overflow = "hidden";
+  }
+
+  function closeModal() {
+    modal.classList.add("hide");
+    modal.classList.remove("show");
+    //modal.classList.toggle("show");
+    // turn back scroll
+    document.body.style.overflow = "";
+  }
+
+  modalOpenBtns.forEach((btn) => {
+    btn.addEventListener("click", () => {
+      openModal();
+    });
+  });
+
+  modalCloseBtn.addEventListener("click", () => {
+    closeModal();
+  });
+
+  modal.addEventListener("click", (e) => {
+    if (e.target === modal) {
+      closeModal();
+    }
+  });
+
+  document.addEventListener("keydown", (e) => {
+    if (e.code === "Escape" && modal.classList.contains("show")) {
+      closeModal();
+    }
+  });
 });
