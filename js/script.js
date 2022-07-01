813c2bd5922c4bd436597b810ef8dd42ebf7a19a
r4sk01 <limbus727@gmail.com>
2022-06-30 19:08:58 -0600

    Added Promotion timer in the bottom of the page. Fix issue if date is passed, timer will show 00-00-00.

--- js/script.js
+++ js/script.js
@@ -1,5 +1,5 @@
 window.addEventListener("DOMContentLoaded", () => {
-  console.log("DOM Content Loaded");
+  // Tabs Content
 
   const tabs = document.querySelectorAll(".tabheader__item"),
     tabsContent = document.querySelectorAll(".tabcontent"),
@@ -22,6 +22,10 @@ window.addEventListener("DOMContentLoaded", () => {
     tabs[i].classList.add("tabheader__item_active");
   }
 
+  // initial init
+  hideTabContent();
+  showTabContent();
+
   tabsParent.addEventListener("click", (event) => {
     if (event.target && event.target.matches("div.tabheader__item")) {
       tabs.forEach((item, i) => {
@@ -33,7 +37,71 @@ window.addEventListener("DOMContentLoaded", () => {
     }
   });
 
-  // initial init
-  hideTabContent();
-  showTabContent();
+  // Timer
+  let dt = new Date();
+  // set deadline 7 days ahead from today
+  let timeSt = dt.setDate(dt.getDate() + 7);
+  let fullDt = new Date(timeSt);
+  // format like 2022-07-07
+  let deadline = fullDt.toLocaleDateString("sv");
+  console.log(deadline);
+
+  function getTimeRemaining(endtime) {
+    let days, seconds, minutes, hours;
+    const t = Date.parse(endtime) - Date.parse(new Date());
+
+    if (t <= 0) {
+      days = 0;
+      hours = 0;
+      minutes = 0;
+      seconds = 0;
+    } else {
+      days = Math.floor(t / (1000 * 60 * 60 * 24));
+      seconds = Math.floor((t / 1000) % 60);
+      minutes = Math.floor((t / 1000 / 60) % 60);
+      hours = Math.floor((t / (1000 * 60 * 60)) % 24);
+    }
+
+    return {
+      total: t,
+      days: days,
+      hours: hours,
+      minutes: minutes,
+      seconds: seconds,
+    };
+  }
+
+  function getZero(num) {
+    if (num >= 0 && num < 10) {
+      return "0" + num;
+    } else {
+      return num;
+    }
+  }
+
+  function setClock(selector, endtime) {
+    const timer = document.querySelector(selector),
+      days = timer.querySelector("#days"),
+      hours = timer.querySelector("#hours"),
+      minutes = timer.querySelector("#minutes"),
+      seconds = timer.querySelector("#seconds"),
+      timeInterval = setInterval(updateClock, 1000);
+
+    updateClock();
+
+    function updateClock() {
+      const t = getTimeRemaining(endtime);
+
+      days.innerHTML = getZero(t.days);
+      hours.innerHTML = getZero(t.hours);
+      minutes.innerHTML = getZero(t.minutes);
+      seconds.innerHTML = getZero(t.seconds);
+
+      if (t.total <= 0) {
+        clearInterval(timeInterval);
+      }
+    }
+  }
+
+  setClock(".timer", deadline);
 });
